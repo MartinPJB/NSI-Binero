17ae3a4280538a54076e31c6ebd8922223ae1af5
Co_Zax <sushibar2929@gmail.com>
2021-01-07 21:38:41 +0100

    Update binero.py
    Ajout de la fonction "transpose"

--- binero.py
+++ binero.py
@@ -1,12 +1,9 @@
+#Fonction pour transposer un tableau
 def transpose(g):
-    resultat = g
-    
-    colonne = []
-    for i in range(len(g)):
-        for j in range(len(g[i])):
-            colonne.append(g[j][i])
-            print(i, j)
-            
-    print(colonne)
-        
-transpose([["1","0","1","0"],["1","0","1","0"],["0","1","1","0"],["1","1","0","0"]])
+    resultat = [[""] * len(i) for i in g] #Va créer un tableau contenant le même nombre d'éléments que g mais vide
+    for i in range(len(g[0])): #Va parcourir chacun des éléments de g
+        for j in range(len(g)): #Va parcourir les tableaux de tableau de g (ses éléments)
+            resultat[j][i] = g[i][j] #Va ensuite entrer les valeurs dans le tableau "resultat"
+    return resultat
+
+assert transpose([["1","2","3","4"],["5","6","7","8"],["9","10","11","12"],["13","14","15","16"]]) == [['1', '5', '9', '13'], ['2', '6', '10', '14'], ['3', '7', '11', '15'], ['4', '8', '12', '16']]
