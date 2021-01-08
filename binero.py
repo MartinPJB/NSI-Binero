b5504c7515262cf1e041b3f5578ef3002d472596
Co_Zax <sushibar2929@gmail.com>
2021-01-08 17:57:33 +0100

    Ajout de "verifie_parite(liste)"
    A optimiser

--- binero.py
+++ binero.py
@@ -23,10 +23,38 @@ def transpose(g):
         for j in range(len(g)):             # Parcours des éléments de la liste g[i] (rappel: ici, len(g) == len(g[i]))
             resultat[j][i] = g[i][j]        # Remplace les valeurs vides du tableau de resultat en inversant les
                                             # positions afin d'inverser les colonnes et les lignes de la grille
-
     return resultat
-
 
 assert transpose([["1", "2", "3", "4"], ["5", "6", "7", "8"], ["9", "10", "11", "12"], ["13", "14", "15", "16"]]) == \
                  [["1", "5", "9", "13"], ["2", "6", "10", "14"], ["3", "7", "11", "15"], ["4", "8", "12", "16"]]
 assert transpose([[34, True], [[False, "25"], 0]]) == [[34, [False, '25']], [True, 0]]
+
+
+def verifie_parite(liste):
+    """Fonction qui, à l'aide d'une grille sous forme de liste, retourne True si celle-ci contient autant de 0 que de 1,
+    Sinon retourne False.
+
+    Paramètre(s):
+        - liste list: Grille à vérifier
+
+    Retourne:
+        - boolean: Résultat de la vérification de parité.
+
+    Exemples:
+        >>> verifie_parite(["1","0","1","0"])
+        True
+
+        >>> verifie_parite(["1","1","1","0"])
+        False
+    """
+    zero, un = 0, 0
+    for i in liste:
+        if i == "1":
+            un += 1
+        else:
+            zero += 1
+    if zero == un:
+        return True
+    return False
+
+print(verifie_parite(["1","1","1","0"]))
